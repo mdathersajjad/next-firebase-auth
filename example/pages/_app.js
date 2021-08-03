7181cae583192ab94d32091e0ba97607d59a0a85
Kevin Jennison <kevin.jennison1@gmail.com>
2021-08-03 11:07:03 -0400

    Add Firebase w/o NFA

--- example/pages/_app.js
+++ example/pages/_app.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import '../styles/globals.css'
 
+// eslint-disable-next-line no-unused-vars
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
